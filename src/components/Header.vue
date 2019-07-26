77107364d15c570524b27b28cceb3a85b32edde4
1056996785@qq.com <hhr147258forever>
2019-07-26 11:47:29 +0800

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header >
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="py-2" href="#">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
+      </a>
+      <a href="/" class="nav-brand">Pizza点餐系统</a>
+      <!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>-->
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active title"><a class="nav-link" href="#">主页</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">菜单</a> </li>
+          <li class="nav-item"><a class="nav-link" href="#">管理</a> </li>
+          <li class="nav-item"><a class="nav-link" href="#">关于我们</a> </li>
+        </ul>
+
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item"><a class="nav-link" href="#">登录</a> </li>
+          <li class="nav-item"><a class="nav-link" href="#">注册</a> </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+<script>
+
+</script>
+<style>
+  .title{
+    margin-left: 5px;
+  }
+</style>
