54ad9a5ccfc585e3cb8347b0aecbea712179eca4
1056996785@qq.com <hhr147258forever>
2019-07-26 18:00:48 +0800

    新增路由全局守卫

--- src/main.js
+++ src/main.js
@@ -7,6 +7,17 @@ import router from './router'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+//实现全局守卫
+router.beforeEach((to,from,next)=>{
+  //alert("还没登录，请先登录");
+  //判断 store.gettes.isLogin === false
+  if(to.path === "/login" || to.path === "/register"){
+    next();
+  }else{
+    alert("还没登录，请先登录");
+  }
+
+})
 new Vue({
   el: '#app',
   router,
